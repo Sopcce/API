2df82ad23f2d690a8c30024af1f9704c65e43ea3
guojiaqiu <sopcce@qq.com>
2019-11-17 12:26:05 +0800

    add

---
+++ Aliyun.OSS/Aliyun.OSS/Config.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Configuration;
+using Aliyun.OSS.Common;
+
+namespace Aliyun.OSS.Samples
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Config
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static string productVersion = ConfigurationManager.AppSettings["ProductVersion"];
+        /// <summary>
+        /// 
+        /// </summary>
+        public static string bucketName = ConfigurationManager.AppSettings["AliyunOSS.BucketName"];
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static string Endpoint = ConfigurationManager.AppSettings["AliyunOSS.Endpoint.API"];
+
+        /// <summary>
+        ///  Access Key ID 
+        ///  https://ak-console.aliyun.com/#/accesskey
+        /// </summary>
+        public static string AccessKeyId = ConfigurationManager.AppSettings["AliyunOSS.AccessKeyID"];
+
+        /// <summary>
+        /// Access Key Secret
+        /// https://ak-console.aliyun.com/#/accesskey
+        /// </summary>
+        public static string AccessKeySecret = ConfigurationManager.AppSettings["AliyunOSS.AccessKeySecret"];
+        /// <summary>
+        /// 
+        /// </summary>
+ 
+        public static string DirToDownload = Endpoint+ "/DirToDownload";
+
+        public static string FileToUpload = Endpoint+ "/FileToUpload";
+
+        public static string BigFileToUpload = Endpoint + "/BigFileToUpload";
+    }
+}
